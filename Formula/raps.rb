bd9d85ba4304ef4b87a31f0f0ac0029da7c841d4
Dmytro Yemelianov <dyd@sitecore.net>
2025-12-26 16:48:32 +0200

    chore: update checksums for v2.0.0

--- Formula/raps.rb
+++ Formula/raps.rb
@@ -7,20 +7,20 @@ class Raps < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/dmytro-yemelianov/raps/releases/download/v#{version}/raps-macos-x64.tar.gz"
-      # sha256 "..." (to be updated by CI)
+      sha256 "df2b32e0cb9586cb9f2630795541b45263e45540d55ab0fa7f5c24b126a4a63a"
     else
       url "https://github.com/dmytro-yemelianov/raps/releases/download/v#{version}/raps-macos-arm64.tar.gz"
-      # sha256 "..." (to be updated by CI)
+      sha256 "1a4fda2fd8adfc6da49566a36f7cb83a4d12fe29c04229cc71271ceafe8e34fa"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/dmytro-yemelianov/raps/releases/download/v#{version}/raps-linux-x64.tar.gz"
-      # sha256 "..." (to be updated by CI)
+      sha256 "d7c88417bb486bb64b3ec6f49e95cfc2fcb1483bd8739629bb350932ec88dd84"
     else
       url "https://github.com/dmytro-yemelianov/raps/releases/download/v#{version}/raps-linux-arm64.tar.gz"
-      # sha256 "..." (to be updated by CI)
+      sha256 "a4a24320c7a276ebda8fb099d819ff040ae7911d5973b0cb575aaaea8e3d361f"
     end
   end
 
